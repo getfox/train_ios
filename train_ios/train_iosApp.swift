b031ae84d34c7dafe0945c3bbea627e8f799de61
getfox <getfox@outlook.com>
2023-11-05 11:05:27 +0800

    Initial Commit

---
+++ train_ios/train_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  train_iosApp.swift
+//  train_ios
+//
+//  Created by 郭旭 on 2023/11/5.
+//
+
+import SwiftUI
+
+@main
+struct train_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
